dfdb515f66824e7559907d8e8eeacadacf1db3ca
dell <dell@DESKTOP-FGTAPG2>
2019-06-11 11:41:33 +0530

    User Pojo Created.!

---
+++ src/main/java/com/pojos/User.java
@@ -0,0 +1,10 @@
+package com.pojos;
+
+public class User {
+	private Integer userId;
+	private String userName;
+	private String emailId;
+	private String mobileNumber;
+	private String Address;
+	
+}
