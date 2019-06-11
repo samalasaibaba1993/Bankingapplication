be55593df459093d90c8393651cbd65f4ea49206
dell <dell@DESKTOP-FGTAPG2>
2019-06-11 12:22:48 +0530

    Created setter and getters

--- src/main/java/com/pojos/User.java
+++ src/main/java/com/pojos/User.java
@@ -6,5 +6,37 @@ public class User {
 	private String emailId;
 	private String mobileNumber;
 	private String Address;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+	
 	
 }
